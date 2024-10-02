e478622849a3b28aa584dd69c4fbe79b11b62959
Srujana katla <116832145+Srujanakatla@users.noreply.github.com>
2024-10-02 11:38:50 +0530

    Add files via upload

---
+++ 30thsep/loops.js
@@ -0,0 +1,66 @@
+//if-elseif-else 
+var a ="srujana";
+var b = "katla";
+ 
+if(a==b){
+    console.log("a==b srujana katla");
+    
+}
+else if(a!==b){
+    console.log("a!==b srujana katla");
+    
+}
+else if(a>=b){
+    console.log("a>=b srujana katla");
+    
+}
+else if(a<=b){
+    console.log("a<=b srujana katla");
+    
+}
+else{
+    console.log("string srujana katla");
+    
+}
+
+//if-else
+var num = 10;
+if(num){
+    console.log("ifelse:","even");
+    
+}
+else if(num){
+    console.log("ifelse:","odd");
+    
+}
+
+//loops
+
+for(var a=1; a<=5; a++){
+    console.log("postincr:",a);
+    
+}
+
+//other ex of loops
+// for(var b = 5; b>=5; b++){
+//     console.log(b);
+    
+// }
+for(var c = 10; c>=1; c--){
+        console.log("postdecr:",c)
+    }
+for (var d = 20; d<=5; ++d){
+    console.log("preincr:",d);
+    
+}
+
+for(var e = 30; e>=10; --e){
+    console.log("predecr:",e);
+    
+}
+
+
+// for(var f = 2; f<=4; f--){
+//     console.log("sorry");
+    
+// }
---
+++ 30thsep/task.js
@@ -0,0 +1,66 @@
+for(var a=1;a<=10;a++){
+    console.log("question 1:",a);   
+} 
+
+var b = 1;
+while(b<=10){
+    b++;
+    console.log("question 1 while:",b);
+    
+}
+
+var c = 1;
+do{
+    c++;
+    console.log("question 1 do-while:",c);   
+}
+while(c<=10);
+
+for(d=2;d<=20;d+=2){
+    console.log("question 2 for loop:",d);
+}
+
+let s = 2;
+while (s <= 10) {
+    console.log("question 2 for while:",s);
+    s += 2;
+}
+
+
+var f = 2;
+do{
+    console.log("question 2 do-while:",f);
+    f+=2;
+}
+while(f<=20);
+
+for(var i=1;i<=20;i++){
+    console.log("question 3 loop:",i); 
+}
+
+var j=1;
+while (i < 20) {
+    j += 2;
+    console.log("question 3 while:",j);
+}
+
+var k = 1;
+do {
+    console.log("question 3 do-while:",k);
+    k+=2;
+}
+while (k < 20) {
+    
+}
+
+var marks = 40;
+if(marks >= 35 && marks <= 45){
+    console.log("grade:D");
+    
+}
+else{
+    console.log("grade:not D");
+    
+}
+
+
