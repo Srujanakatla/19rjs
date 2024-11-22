48a72e68be94b4f08465eaa87dbb94446f4e09df
Srujana katla <116832145+Srujanakatla@users.noreply.github.com>
2024-11-22 17:01:50 +0530

    Add files via upload

---
+++ 20thnov/fakestore.js
@@ -0,0 +1,34 @@
+async function productData() {
+    let data = await fetch('https://fakestoreapi.com/products?limit=5');
+    let productData = await data.json();
+    let filterData = productData.filter(
+      (item) => item.price > 10 && item.rating.rate >  1
+    );
+      console.log(filterData);
+    displayData(filterData);
+}
+
+//   }productData()
+  
+  function displayData(items) {
+    let mainContainer = document.createElement("div");
+    mainContainer.style.cssText =
+      "display: flex; justify-content: space-between; flex-wrap: wrap; gap: 20px; padding: 20px; background-color:grey";
+  
+    items.forEach((item) => {
+      let container = document.createElement("div");
+      container.style.cssText =
+        "border: 1px solid blue; color:white;border-radius: 8px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); padding: 15px; text-align: center; width: 300px;";
+  
+      container.innerHTML += `<img src=${item.image} style="width: 200px; height:200px; border-radius: 8px;">
+      <h2 style="font-size: 18px;">${item.title}</h2>
+      <p style="font-size: 16px";>$${item.price}</p>
+      <h3 style="font-size: 14px;">${item.rating.rate}</h3>`;
+  
+      mainContainer.appendChild(container);
+      document.body.appendChild(mainContainer);
+    //   console.log(items);
+      
+    });
+  }
+  productData();
